07c19b015578675be08affc8f2d7f6e7b4a0e004
liana-arteta <liana.arteta.heins@gmail.com>
2026-06-09 13:46:15 +0200

    Creación con spring y maven

---
+++ src/main/java/com/liana/coffeshop_vigo/CoffeshopVigoApplication.java
@@ -0,0 +1,13 @@
+package com.liana.coffeshop_vigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeshopVigoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeshopVigoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/liana/coffeshop_vigo/CoffeshopVigoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liana.coffeshop_vigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeshopVigoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
